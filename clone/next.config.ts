ed07beab36b89e2293be983ec0f7ef05b7c5529a
Maria Lukowich <mlukowich27@gmail.com>
2025-03-18 15:01:33 -0600

    updating the next.config file

--- clone/next.config.ts
+++ clone/next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'media.istockphoto.com',
+            pathname: '**',
+        },
+        {
+            protocol: 'https',
+            hostname: 't3.ftcdn.net',
+            pathname: '**',
+        },
+        {
+            protocol: 'https',
+            hostname: 'necessary-ladybug-115.convex.cloud',
+            pathname: '**',
+        },
+        {
+            protocol: 'https',
+            hostname: 'upload.wikimedia.org',
+            pathname: '**',
+        },
+        {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+            pathname: '**',
+        },
+    ],
+  },
 };
 
 export default nextConfig;
